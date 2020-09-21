1e9551c31f94a54dc84877bfa3775940e3a070e6
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-09-21 15:33:18 +0200

    Add deleteProduct method to productController.js and routes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,5 +5,6 @@ router.post('/', productController.createProduct.bind(productController))
 router.get('/', productController.getAllProducts.bind(productController))
 router.get('/:id', productController.getSpecificProduct.bind(productController))
 router.patch('/:id', productController.updateProduct.bind(productController))
+router.delete('/:id', productController.deleteProduct.bind(productController))
 
 module.exports = router
