fe0e812f29b8a09f9253adaa943cdc01ae4d74fc
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-09-21 13:28:18 +0200

    Change post to get on product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,6 +2,6 @@ const productController = require('../controllers/productController')
 const router = require('express').Router()
 
 router.post('/', productController.createProduct.bind(productController))
-router.post('/:id', productController.getSpecificProduct.bind(productController))
+router.get('/:id', productController.getSpecificProduct.bind(productController))
 
 module.exports = router
