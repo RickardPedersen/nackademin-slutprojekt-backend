bfe0ce97f37e59bb83db6597982315659c73d07b
Rickard Pedersen <pedersen.rickard@outlook.com>
2020-09-21 16:29:59 +0200

    create order integration tests

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,33 @@
 const Order = require('../models/orderModel')
+const Product = require('../models/productModel')
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 class OrderController {
     async addOrder(req, res, next) {
         try {
-            const { items, orderValue } = req.body
-            res.status(201).json(await Order.createOrder({items, orderValue}))
+            const { items } = req.body
+            let orderValue = 0
+            for (let productId of items) {
+                const product = await Product.getSpecificProduct(productId)
+                orderValue += product.price
+            }
+
+            const order = await Order.createOrder({items, orderValue})
+            if (!req.headers.authorization) { return res.status(201).json(order) }
+            const token = req.headers.authorization.replace("Bearer ", "")
+            let payload;
+
+            try {
+                payload = jwt.verify(token, process.env.SECRET)
+            } catch (error) {
+                return res.status(201).json(order)
+            }
+
+            const orderId = order._id.toString()
+            await User.addOrderHistory(payload.id, orderId)
+
+            res.status(201).json(order)
         } catch (error) {
             next(error)
         }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,6 @@
 const orderController = require('../controllers/orderController')
 const router = require('express').Router()
+const {user} = require('../middlewares/handleAuth')
 
 //router.get('/', orderController.example)
 router.post('/', orderController.addOrder)
